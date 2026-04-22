9c412899bfdb3263ab3f30429eaa9f75f0694bf0
Ath Achcha <athachcha.siri@gmail.com>
2026-04-23 01:22:21 +0700

    Up to Date

--- Gen_data/EDIT_auth_user.py
+++ Gen_data/EDIT_auth_user.py
@@ -6,29 +6,27 @@ from datetime import datetime, timedelta, timezone
 # USERS
 # ------------------------
 
-access = []
-
-def generate_pg_timestamp():
+def generate_timestamp():
     dt = datetime(2025,1,1, tzinfo=timezone.utc) + timedelta(
-        days=random.randint(0,365),
-        hours=random.randint(0,23),
+        days=random.randint(0,120),
+        hours=random.randint(8,18),
         minutes=random.randint(0,59),
-        seconds=random.randint(0,59),
-        microseconds=random.randint(0,999999)
+        seconds=random.randint(0,59)
     )
-    
-    # format ให้ตรง PostgreSQL
-    return dt.strftime("%Y-%m-%d %H:%M:%S.%f+00")
+    return dt.strftime("%Y-%m-%d %H:%M:%S+00")
 
-for i in range(1,501):
-    access.append({
-        "log_id": f"L{i:05}",
+data = []
+
+for i in range(1,201):
+    data.append({
+        "usege_id": f"USE{i:03}",
+        "inv_id": f"INV{random.randint(1,20):03}",
         "user_id": random.randint(1,50),
-        "access_time": generate_pg_timestamp(),
-        "access_type": random.choice(["Check-in","Check-out"])
+        "usege_date": generate_timestamp(),
+        "value_use": round(random.uniform(0.5,5.0),2)
     })
 
-access_df = pd.DataFrame(access)
-access_df.to_csv("lab_access_new.csv", index=False)
+df = pd.DataFrame(data)
+df.to_csv("chemical_usege.csv", index=False)
 
-print("Dataset generated successfully!")
+print("Done!")
